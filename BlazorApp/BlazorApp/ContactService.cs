fbddb43657870fd312e652b909008009304c2cd8
ibharti <bhartibhardwaj1207@gmail.com>
2024-04-30 15:48:52 +0200

    DI : singleton service

---
+++ BlazorApp/BlazorApp/ContactService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp
+{
+    public class ContactService
+    {
+        public List<Contact> ContactList = new List<Contact>();
+        public void AddContact (Contact contact)
+        {
+            ContactList.Add(contact);
+        }
+    }
+}
--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -1,3 +1,4 @@
+using BlazorApp;
 using BlazorApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -5,9 +6,17 @@ using Microsoft.AspNetCore.Components.Web;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+ConfigureServices(builder.Services);
+
+// We can move the below mentioned services into the configure services function
+//builder.Services.AddRazorPages();
+//builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<WeatherForecastService>();
+
+//method 1 of adding singleton service
+
+//builder.Services.AddSingleton<ContactService>();
 
 var app = builder.Build();
 
@@ -26,3 +35,13 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+//other method for singletonservice
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<ContactService>();
+    services.AddRazorPages();
+    services.AddServerSideBlazor();
+    services.AddSingleton<WeatherForecastService>();
+}
